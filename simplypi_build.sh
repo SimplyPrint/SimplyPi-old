58d37799e92035bb33952a1e14675de1e0a79960
AlbertMN <albert@mollernielsen.dk>
2020-05-24 13:24:09 +0200

    Fix

--- simplypi_build.sh
+++ simplypi_build.sh
@@ -1,5 +1,3 @@
-sudo wget -O - https://simplyprint.dk/software/install_system.sh src/variants/SimplyPi/home/pi/SimplyPrint | sudo bash
-
 cd src/variants/SimplyPi/filesystem/home/pi
 
 rm -rf SimplyPrint
@@ -11,3 +9,4 @@ unzip updated_file.zip &&
 sudo rm -rf updated_file.zip &&
 sudo chmod -R 757 ../SimplyPrint &&
 sudo /home/pi/oprint/bin/python2 -m pip --disable-pip-version-check install SimplyPrint-OctoPrint-Plugin.zip --no-cache-dir && \\
+sudo rm -rf SimplyPrint-OctoPrint-Plugin.zip
