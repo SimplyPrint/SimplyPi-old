e8848e1b7d0bb6b04c0aa59d0854523c842ace43
AlbertMN <albert@mollernielsen.dk>
2020-05-24 13:24:22 +0200

    Fix 2

--- simplypi_build.sh
+++ simplypi_build.sh
@@ -8,5 +8,5 @@ curl -sS https://simplyprint.dk/software/SimplyPrint-files.zip > updated_file.zi
 unzip updated_file.zip &&
 sudo rm -rf updated_file.zip &&
 sudo chmod -R 757 ../SimplyPrint &&
-sudo /home/pi/oprint/bin/python2 -m pip --disable-pip-version-check install SimplyPrint-OctoPrint-Plugin.zip --no-cache-dir && \\
-sudo rm -rf SimplyPrint-OctoPrint-Plugin.zip
+#sudo /home/pi/oprint/bin/python2 -m pip --disable-pip-version-check install SimplyPrint-OctoPrint-Plugin.zip --no-cache-dir && \\
+#sudo rm -rf SimplyPrint-OctoPrint-Plugin.zip
