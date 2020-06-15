fb4818e2850122872c0505e278c1048355fdc1e1
AlbertMN <albert@mollernielsen.dk>
2020-06-15 22:00:19 +0200

    Startup service hostname change fix & better debugging output

--- src/variants/SimplyPi/filesystem/home/root/bin/simplyprint_start.sh
+++ src/variants/SimplyPi/filesystem/home/root/bin/simplyprint_start.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-echo "starting up simplyprint_start.sh"
+echo "SimplyPi setup service started"
 # python3 /home/pi/SimplyPrint/crontab_manager.py -s > /dev/null 2>&1
 python3 /home/pi/SimplyPrint/crontab_manager.py -s
 
 search=octopi
 if grep -q $search "/etc/hostname"; then
-  replace="SimplyPrint-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)"
-  sed -i "s/${search}/${replace}/g" /etc/hostname
-  sed -i "s/${search}/${replace}/g" /etc/hosts
-  reboot
+        echo "Hostname is 'octopi' - gonna change it"
+        replace="SimplyPi-$(openssl rand -hex 2)"
+
+        sed -i "s/${search}/${replace}/g" /etc/hostname
+        sed -i "s/${search}/${replace}/g" /etc/hosts
+
+        echo "Hostname changed. Rebooting."
+        reboot
 fi
+
+echo "SimplyPi setup service done"
