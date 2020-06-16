cca1de83a319d5b54a53a9a9d389404f631fd439
AlbertMN <albert@mollernielsen.dk>
2020-06-16 22:37:25 +0200

    Added automatic install of OctoPrint plugin and for the service to be started after OctoPrint

--- src/variants/SimplyPi/filesystem/home/root/bin/simplyprint_start.sh
+++ src/variants/SimplyPi/filesystem/home/root/bin/simplyprint_start.sh
@@ -4,16 +4,25 @@ echo "SimplyPi setup service started"
 # python3 /home/pi/SimplyPrint/crontab_manager.py -s > /dev/null 2>&1
 python3 /home/pi/SimplyPrint/crontab_manager.py -s
 
+echo "Checking if the OctoPrint plugin needs to be installed..."
+FILE=/home/pi/SimplyPrint/SimplyPrint-OctoPrint-Plugin.zip
+if test -f "$FILE"; then
+  echo "OctoPrint plugin needs to be installed"
+  sudo /home/pi/oprint/bin/python2 -m pip --disable-pip-version-check install $FILE --no-cache-dir &&
+  sudo service octoprint restart
+  echo "Should have installed plugin and restarted the OctoPrint service"
+fi
+
 search=octopi
 if grep -q $search "/etc/hostname"; then
-        echo "Hostname is 'octopi' - gonna change it"
-        replace="SimplyPi-$(openssl rand -hex 2)"
+  echo "Hostname is 'octopi' - gonna change it"
+  replace="SimplyPi-$(openssl rand -hex 2)"
 
-        sed -i "s/${search}/${replace}/g" /etc/hostname
-        sed -i "s/${search}/${replace}/g" /etc/hosts
+  sed -i "s/${search}/${replace}/g" /etc/hostname
+  sed -i "s/${search}/${replace}/g" /etc/hosts
 
-        echo "Hostname changed. Rebooting."
-        reboot
+  echo "Hostname changed. Rebooting."
+  reboot
 fi
 
 echo "SimplyPi setup service done"
