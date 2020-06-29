d489ba6fc876f100a13d9cd38a62fc1271ea0e95
Albert Nielsen <albert@mollernielsen.dk>
2020-06-29 19:30:44 +0200

    Made the SimplyPi build script more intelligent

--- simplypi_build.sh
+++ simplypi_build.sh
@@ -1,21 +1,35 @@
 #!/usr/bin/env bash
 
 yes | sudo apt-get install gawk util-linux qemu-user-static git p7zip-full python3
-git pull
+sudo -u $SUDO_USER git pull
 cd ../
-git clone https://github.com/guysoft/CustomPiOS.git
-cd SimplyPi
 
-cd src/variants/SimplyPi/filesystem/home/pi
+CUSTOMPIOS_CHECK=CustomPiOS
+
+if [ -d "$CUSTOMPIOS_CHECK" ]; then
+  printf "Pulling for latest CustomPiOS"
+  cd $CUSTOMPIOS_CHECK
+  sudo -u $SUDO_USER git pull
+  cd ../
+  printf "Pulled"
+else
+  printf "Cloning CustomPiOS..."
+  sudo -u $SUDO_USER git clone https://github.com/guysoft/CustomPiOS.git
+  printf "Cloned"
+fi
+
+cd SimplyPi/src/image
+wget -c --trust-server-names 'https://downloads.raspberrypi.org/raspios_lite_armhf_latest'
+cd ../variants/SimplyPi/filesystem/home/pi
 
 rm -rf SimplyPrint
 mkdir SimplyPrint &&
-sudo chmod -R 757 SimplyPrint &&
-cd SimplyPrint &&
-curl -sS https://simplyprint.dk/software/SimplyPrint-files.zip > updated_file.zip &&
-unzip updated_file.zip &&
-sudo rm -rf updated_file.zip &&
-sudo chmod -R 757 ../SimplyPrint
+  sudo chmod -R 757 SimplyPrint &&
+  cd SimplyPrint &&
+  curl -sS https://simplyprint.dk/software/SimplyPrint-files.zip >updated_file.zip &&
+  unzip updated_file.zip &&
+  sudo rm -rf updated_file.zip &&
+  sudo chmod -R 757 ../SimplyPrint
 
 cd /home/pi/SimplyPi/src
 ../../CustomPiOS/src/update-custompios-paths
